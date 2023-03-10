67b6ff10a0c8e311fa356abf60e64f956d597640
Arne Halvorsen <arha@dips.no>
2023-03-10 18:20:27 +0100

    wip: added specification project, specified ICustomer and made default implementation in domain project

---
+++ src/InMetaCase.Domain/Model/Customer.cs
@@ -0,0 +1,24 @@
+﻿using InMetaCase.Specification.Model;
+
+namespace InMetaCase.Domain.Model;
+
+public record class Customer : ICustomer
+{
+    public int Id { get; set; }
+    public string? Email { get; set; }
+
+    public string? Title { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+
+    public string? StreetNumber { get; set; }
+    public string? BuildingName { get; set; }
+    public string? StreetName { get; set; }
+    public string? StreetType { get; set; }
+    public string? Direction { get; set; }
+    public string? City { get; set; }
+    public string? Region { get; set; }
+    public string? PostalCode { get; set; }
+    public string? Country { get; set; }
+    public string? Phone { get; set; }
+}
---
+++ src/InMetaCase.Specification/Model/ICustomer.cs
@@ -0,0 +1,25 @@
+﻿namespace InMetaCase.Specification.Model
+{
+    public interface ICustomer
+    {
+        int Id { get; }
+
+        string? Email { get; }
+
+        string? Title { get; }
+
+        string? FirstName { get; }
+        string? LastName { get; }
+
+        string? StreetNumber { get; }
+        string? BuildingName { get; }
+        string? StreetName { get; }
+        string? StreetType { get; }
+        string? Direction { get; }
+        string? City { get; }
+        string? Region { get; }
+        string? PostalCode { get; }
+        string? Country { get; }
+        string? Phone { get; }
+    }
+}
