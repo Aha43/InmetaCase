abba37866129acc4888c0b02a8b2136be17bf744
Arne Halvorsen <arha@dips.no>
2023-03-10 19:37:38 +0100

    wip: defined crud api

---
+++ src/InMetaCase.Specification/Domain/Api/ICrudApi.cs
@@ -0,0 +1,10 @@
+﻿namespace InMetaCase.Specification.Domain.Api
+{
+    public interface ICrudApi<T> where T : class
+    {
+        Task<T> CreateAsync<P>(P param, CancellationToken cancellationToken) where P : class;
+        Task<IEnumerable<T>> ReadAsync<P>(P param, CancellationToken cancellationToken) where P : class;
+        Task<T> UpdateAsync<P>(P param, CancellationToken cancellationToken) where P : class;
+        Task<T> DeleteAsync<P>(P param, CancellationToken cancellationToken) where P : class;
+    }
+}
