b2edfc1d82d5b98deef38193f9bc542ee2c930e8
Arne Halvorsen <arha@dips.no>
2023-03-11 09:16:45 +0100

    wip: added sql database project

---
+++ src/Inmeta.Infrastructure.Database/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using InmetaCase.Domain.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Inmeta.Infrastructure.Database
+{
+    public class DataContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public DataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to sql server with connection string from app settings
+            options.UseSqlServer(Configuration.GetConnectionString("WebApiDatabase"));
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+
+    }
+
+}
