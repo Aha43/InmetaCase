6447b9c8605358d60931e1c0c6aa38da535fcd27
Arne Halvorsen <arha@dips.no>
2023-03-11 11:13:40 +0100

    wip: added some utilities to crosscutting (config stuff) about to use

---
+++ src/InmetaCase.Crosscutting/ServiceCollectionExtensionMethods.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InmetaCase.Crosscutting
+{
+    public static class ServiceCollectionExtensionMethods
+    {
+        public static IServiceCollection AddFromConfigurationAs<T>(IServiceCollection services, IConfiguration configuration) where T : class
+        {
+            var service = configuration.GetAs<T>();
+            if (service != null) services.AddSingleton(service);
+            return services;
+        }
+
+        public static IServiceCollection AddFromConfigurationRequiredAs<T>(IServiceCollection services, IConfiguration configuration) where T : class
+        {
+            var service = configuration.GetRequiredAs<T>();
+            return services.AddSingleton(service);
+        }
+
+    }
+
+}
