6cfb0799a696c4ca36f3470940f41191bca900ef
Arne Halvorsen <arha@dips.no>
2023-03-11 19:13:56 +0100

    wip: made web api controllers for address and order, pending testing

---
+++ src/InmetaCase.Application.WebApi/Controllers/AddressController.cs
@@ -0,0 +1,40 @@
+﻿using InmetaCase.Domain.Model;
+using InmetaCase.Specification.Api;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InmetaCase.Application.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AddressController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        private readonly IAddressApi _api;
+
+        public AddressController(ILogger<CustomerController> logger, IAddressApi api)
+        {
+            _logger = logger;
+            _api = api;
+        }
+
+        [HttpPost()]
+        public async Task<Address> CreateAsync([FromBody] Address address) =>
+            await _api.CreateAsync(address, default).ConfigureAwait(false);
+
+        [HttpGet()]
+        [HttpGet("{id}")]
+        public async Task<IEnumerable<Address>> ReadAsync([FromRoute] int? id) => 
+            await _api.ReadAsync(id, default).ConfigureAwait(false);
+
+        [HttpPut()]
+        public async Task<Address?> UpdateAsync([FromBody] Address customer) => 
+            await _api.UpdateAsync(customer, default).ConfigureAwait(false);
+
+        [HttpDelete("{id}")]
+        public async Task<Address?> DeleteAsync([FromRoute] int id) => 
+            await _api.DeleteAsync(id, default).ConfigureAwait(false);
+
+    }
+
+}
---
+++ src/InmetaCase.Application.WebApi/Controllers/OrderController.cs
@@ -0,0 +1,37 @@
+﻿using InmetaCase.Domain.Model;
+using InmetaCase.Specification.Api;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InmetaCase.Application.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class OrderController : ControllerBase
+{
+    private readonly ILogger _logger;
+
+    private readonly IOrderApi _api;
+
+    public OrderController(ILogger<OrderController> logger, IOrderApi api)
+    {
+        _logger = logger;
+        _api = api;
+    }
+
+    [HttpPost()]
+    public async Task<Order> CreateAsync([FromBody] Order order) =>
+        await _api.CreateAsync(order, default).ConfigureAwait(false);
+
+    [HttpGet()]
+    [HttpGet("{id}")]
+    public async Task<IEnumerable<Order>> ReadAsync([FromRoute] int? id) =>
+        await _api.ReadAsync(id, default).ConfigureAwait(false);
+
+    [HttpPut()]
+    public async Task<Order?> UpdateAsync([FromBody] Order order) =>
+        await _api.UpdateAsync(order, default).ConfigureAwait(false);
+
+    [HttpDelete("{id}")]
+    public async Task<Order?> DeleteAsync([FromRoute] int id) =>
+        await _api.DeleteAsync(id, default).ConfigureAwait(false);
+}
