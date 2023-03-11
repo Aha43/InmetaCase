32123cdd15d1a146d4429ebc965858029410f572
Arne Halvorsen <arha@dips.no>
2023-03-11 10:52:10 +0100

    wip: fixed name and namesapces in the database infrastructure project and started on the service class

---
+++ src/InmetaCase.Infrastructure.Database/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using InmetaCase.Domain.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace InmetaCase.Infrastructure.Database;
+
+public class DataContext : DbContext
+{
+    protected readonly IConfiguration Configuration;
+
+    public DataContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        // connect to sql server with connection string from app settings
+        options.UseSqlServer(Configuration.GetConnectionString("WebApiDatabase"));
+    }
+
+#nullable disable
+    public DbSet<Address> Addresses { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Order> Orders { get; set; }
+#nullable enable
+
+}
---
+++ src/InmetaCase.Infrastructure.Database/Services.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InmetaCase.Infrastructure.Database;
+
+public static class Services
+{
+    public static IServiceCollection AddInmetaCaseDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
+        throw new NotImplementedException();
+    }
+}
