e1f1528fc4c554ac2cdad6228c8f159c54e2afdb
Arne Halvorsen <arha@dips.no>
2023-03-12 10:24:56 +0100

    wip: got customer to show up ugly in web app, using db api since got problems with http api

---
+++ src/InmetaCase.Application.WebApp/Pages/Application/Customers.razor.cs
@@ -0,0 +1,17 @@
+using InmetaCase.Business.ViewControl;
+using Microsoft.AspNetCore.Components;
+
+namespace InmetaCase.Application.WebApp.Pages.Application
+{
+    public partial class Customers
+    {
+#nullable disable
+        [Inject] private CustomersViewControl ViewControl { get; set; }
+#nullable enable
+
+        protected override async Task OnInitializedAsync()
+        {
+            await ViewControl.Load();
+        }
+    }
+}
