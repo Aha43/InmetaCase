ab7b09581fc86d72d0facc054a799fbaa04d1bf5
Arne Halvorsen <arha@dips.no>
2023-03-13 10:40:00 +0100

    wip: better presentation of customer and orders in lists

--- src/InmetaCase.Application.WebApp/Pages/Application/CustomersPage.razor.cs
+++ src/InmetaCase.Application.WebApp/Pages/Application/CustomersPage.razor.cs
@@ -3,14 +3,11 @@ using Microsoft.AspNetCore.Components;
 
 namespace InmetaCase.Application.WebApp.Pages.Application;
 
-public partial class Customers
+public partial class CustomersPage
 {
 #nullable disable
     [Inject] private CustomersViewController ViewControl { get; set; }
 #nullable enable
 
-    protected override async Task OnInitializedAsync()
-    {
-        await ViewControl.Load();
-    }
+    protected override async Task OnInitializedAsync() => await ViewControl.Load();
 }
--- src/InmetaCase.Business/ViewModelExtensionsMethods.cs
+++ src/InmetaCase.Business/ViewModelExtensionsMethods.cs
@@ -4,8 +4,14 @@ namespace InmetaCase.Business
 {
     public static class ViewModelExtensionsMethods
     {
+        public static string Presentation(this Customer customer) => $"{customer.Id} {customer.FirstName} {customer.LastName}";
         public static string PageUri(this Customer customer) => $"customer/{customer.Id}";
         public static string NewOrderUri(this Customer customer) => $"neworder/{customer.Id}";
+
+        public static string Presentation(this Order order)
+        {
+            return $"{order.Id} {order.OrderDate} {(ServiceType)order.ServiceType}";
+        }
 
         public static string PageUri(this Order order) => $"order/{order.Id}";
     }
